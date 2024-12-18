8ae1dc41c15696518189feca86f2ae782ed7680d
KEVIN-2002L <kevinlim2002sj@gmail.com>
2024-12-18 21:00:46 +0800

    1

---
+++ ShuiGuoXiaoXiaoLe/src/shuiguoxiaoxiaole/modelList/BattleSystem.java
@@ -0,0 +1,13 @@
+package shuiguoxiaoxiaole.modelList;
+
+public class BattleSystem {
+    /**
+     * 开始战斗，并传入金币数量
+     * @param coins 金币数量
+     */
+    public static void startBattleWithCoins(int coins) {
+        System.out.println("进入战斗模式，拥有金币: " + coins);
+        // 这里可以添加战斗逻辑
+        // 例如，初始化战斗场景、加载敌人等
+    }
+}
